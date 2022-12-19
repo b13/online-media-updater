1b7d08227e9eaabbdea3bb764a00f4523618b8a5
Guido Schmechel <guido@ayacoo.de>
2022-12-19 18:26:25 +0100

    [TASK] Init commit for v12 branch

--- Classes/Event/FileListEventListener.php
+++ Classes/Event/FileListEventListener.php
@@ -10,7 +10,7 @@ declare(strict_types=1);
  * of the License, or any later version.
  */
 
-namespace B13\OnlineMediaUpdater\Hooks;
+namespace B13\OnlineMediaUpdater\Event;
 
 use TYPO3\CMS\Core\Imaging\Icon;
 use TYPO3\CMS\Core\Imaging\IconFactory;
@@ -18,48 +18,49 @@ use TYPO3\CMS\Core\Localization\LanguageService;
 use TYPO3\CMS\Core\Page\PageRenderer;
 use TYPO3\CMS\Core\Resource\File;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
-use TYPO3\CMS\Filelist\FileListEditIconHookInterface;
+use TYPO3\CMS\Filelist\Event\ProcessFileListActionsEvent;
 
-class AddOnlineMediaUpdater implements FileListEditIconHookInterface
+final class FileListEventListener
 {
-    /**
-     * @var IconFactory
-     */
-    protected $iconFactory;
+    protected IconFactory $iconFactory;
 
     public function __construct()
     {
         $this->iconFactory = GeneralUtility::makeInstance(IconFactory::class);
     }
 
-    public function manipulateEditIcons(&$cells, &$parentObject): void
+    public function __invoke(ProcessFileListActionsEvent $event): void
     {
+        $actionItems = $event->getActionItems();
+
         $pageRenderer = GeneralUtility::makeInstance(PageRenderer::class);
-        $pageRenderer->loadRequireJsModule('TYPO3/CMS/OnlineMediaUpdater/Backend/Updater');
+        $pageRenderer->loadJavaScriptModule('@online-media-updater/Updater.js');
         $pageRenderer->addInlineLanguageLabelFile('EXT:online_media_updater/Resources/Private/Language/locallang.xlf');
 
-        $fileOrFolderObject = $cells['__fileOrFolderObject'];
+        $fileOrFolderObject = $event->getResource();
         if ($fileOrFolderObject instanceof File) {
             $fileProperties = $fileOrFolderObject->getProperties();
             $extension = $fileProperties['extension'] ?? '';
 
-            $registeredHelpers = $GLOBALS['TYPO3_CONF_VARS']['SYS']['fal']['onlineMediaHelpers'];
+            $registeredHelpers = $GLOBALS['TYPO3_CONF_VARS']['SYS']['fal']['onlineMediaHelpers'] ?? [];
             if (!array_key_exists($extension, $registeredHelpers)) {
                 return;
             }
 
-            $cells['updateOnlineMedia'] = '<a href="#" class="btn btn-default t3js-filelist-update-metadata'
+            $actionItems['tiktok'] = '<a href="#" class="btn btn-default t3js-filelist-update-metadata'
                 . '" data-filename="' . htmlspecialchars($fileOrFolderObject->getName())
                 . '" data-file-uid="' . $fileProperties['uid']
                 . '" title="' . $this->getLanguageService()->getLL('online_media_updater.update') . '">'
                 . $this->iconFactory->getIcon('actions-refresh', Icon::SIZE_SMALL)->render() . '</a>';
         }
+
+        $event->setActionItems($actionItems);
     }
 
     /**
      * @return LanguageService
      */
-    protected function getLanguageService()
+    protected function getLanguageService(): LanguageService
     {
         $languageService = $GLOBALS['LANG'];
         $languageService->includeLLFile('EXT:online_media_updater/Resources/Private/Language/locallang.xlf');
---
+++ Configuration/JavaScriptModules.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'dependencies' => [
+        'core',
+        'backend'
+    ],
+    'tags' => [
+        'online-media-updater.updater',
+    ],
+    'imports' => [
+        '@online-media-updater/' => 'EXT:online_media_updater/Resources/Public/JavaScript/Backend/',
+    ],
+];
