eb5b38b62e285a27f7379fb7e70c672173cf8e46
Achim Fritz <af@achimfritz.de>
2026-03-23 11:05:52 +0100

    [RELEASE] 3.0.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,9 +3,10 @@
 $EM_CONF[$_EXTKEY] = [
     'title' => 'Online Media Updater',
     'description' => 'Update YouTube/Vimeo metadata in fileadmin',
+    'version' => '3.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '11.5.0-12.4.99',
+            'typo3' => '13.4.0-14.99.99',
         ],
     ],
     'autoload' => [
